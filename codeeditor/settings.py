39924ad4acf1d2c7879770df545521d7194534d9
Srabani-09 <srabanibehera1234@gmail.com>
2025-05-19 10:59:05 +0530

    Update setting.py file

--- codeeditor/settings.py
+++ codeeditor/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() in ('true','1','yes')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
